c53cb7174b79450af0807a1ab4101a2c7513f657
tschaell <Tjuk@pool8021x167-250.RZ.HS-Augsburg.DE>
2017-05-23 20:11:28 +0200

    Startscreen
    Startscreen implementiert

---
+++ Zirbl001/app/src/main/java/hsaugsburg/zirbl001/SplashScreen.java
@@ -0,0 +1,32 @@
+package hsaugsburg.zirbl001;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, Tabbar.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+
+        }, SPLASH_TIME_OUT);
+    }
+
+}
