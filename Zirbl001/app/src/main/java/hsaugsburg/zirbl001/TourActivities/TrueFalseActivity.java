f30109f0fa2db30aa79d45eda239f6059e7ce039
Mel <melanie.behrens@hs-augsburg.de>
2017-06-20 17:15:53 +0200

    Update Layout
    Mehrere Anpassungen

--- Zirbl001/app/src/main/java/hsaugsburg/zirbl001/TourActivities/TrueFalseActivity.java
+++ Zirbl001/app/src/main/java/hsaugsburg/zirbl001/TourActivities/TrueFalseActivity.java
@@ -2,9 +2,13 @@ package hsaugsburg.zirbl001.TourActivities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import hsaugsburg.zirbl001.Datamanagement.JSONLetters;
@@ -21,7 +25,37 @@ public class TrueFalseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         new JSONTrueFalse(this).execute("https://zirbl.multimedia.hs-augsburg.de/selectTrueFalseView.php");
         setContentView(R.layout.activity_true_false);
+
+        //Selection
+        Button buttonTruth = (Button) findViewById(R.id.truth);
+        buttonTruth.setOnClickListener(answerTruth);
+        Button buttonLie = (Button) findViewById(R.id.lie);
+        buttonLie.setOnClickListener(answerLie);
+        //
     }
+
+    //Selection
+    View.OnClickListener answerTruth = new View.OnClickListener() {
+        public void onClick(View v) {
+            RelativeLayout selected = (RelativeLayout)findViewById(R.id.truthArea);
+            selected.setBackgroundResource(R.color.colorTruth);
+            ImageView invertedImg = (ImageView)findViewById(R.id.iconTruth);
+            invertedImg.setImageResource(R.drawable.icon_truth_active);
+            Button btA = (Button) findViewById(R.id.truth);
+            btA.setTextColor(Color.WHITE);
+        }
+    };
+    View.OnClickListener answerLie = new View.OnClickListener() {
+        public void onClick(View v) {
+            RelativeLayout selected = (RelativeLayout)findViewById(R.id.lieArea);
+            selected.setBackgroundResource(R.color.colorLie);
+            ImageView invertedImg = (ImageView)findViewById(R.id.iconLie);
+            invertedImg.setImageResource(R.drawable.icon_lie_active);
+            Button btA = (Button) findViewById(R.id.lie);
+            btA.setTextColor(Color.WHITE);
+        }
+    };
+    //
 
     public void continueToNextView(View view) {
         Intent intent = new Intent(mContext, NavigationActivity.class);
