a839013642048a7c11c404b015767b119d5ed0b2
Moritz Preisinger <moritz.preisinger@hs-augsburg.de>
2017-06-26 22:27:22 +0200

    Neustart

---
+++ Zirbl001/app/src/main/java/hsaugsburg/zirbl001/Datamanagement/JSONstationLocation.java
@@ -0,0 +1,8 @@
+package hsaugsburg.zirbl001.Datamanagement;
+
+/**
+ * Created by Mowie on 26.06.17.
+ */
+
+public class JSONstationLocation {
+}
---
+++ Zirbl001/app/src/main/java/hsaugsburg/zirbl001/TourActivities/MapsActivity.java
@@ -0,0 +1,10 @@
+package hsaugsburg.zirbl001.TourActivities;
+
+/**
+ * Created by Mowie on 26.06.17.
+ */
+
+public class MapsActivity {
+
+
+}
