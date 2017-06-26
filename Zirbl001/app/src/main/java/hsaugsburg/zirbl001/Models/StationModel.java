b50cbdf72dbe61f17e9b222aa3874a38785bc15f
Moritz Preisinger <moritz.preisinger@hs-augsburg.de>
2017-06-26 22:35:28 +0200

    stationLocation 01
    auslesen aus DB

---
+++ Zirbl001/app/src/main/java/hsaugsburg/zirbl001/Models/StationModel.java
@@ -0,0 +1,62 @@
+package hsaugsburg.zirbl001.Models;
+
+/**
+ * Created by Mowie on 26.06.17.
+ */
+
+public class StationModel {
+    private int tourID;
+    private int chronologyNumber;
+    private String stationName;
+    private Double latitude;
+    private Double longitude;
+
+
+    public StationModel(int tourID, int chronologyNumber, String stationName, Double latitude, Double longitude) {
+        this.tourID = tourID;
+        this.chronologyNumber = chronologyNumber;
+        this.stationName = stationName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public int getTourID() {
+        return tourID;
+    }
+
+    public void setTourID(int tourID) {
+        this.tourID = tourID;
+    }
+
+    public int getChronologyNumber() {
+        return chronologyNumber;
+    }
+
+    public void setChronologyNumber(int chronologyNumber) {
+        this.chronologyNumber = chronologyNumber;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
