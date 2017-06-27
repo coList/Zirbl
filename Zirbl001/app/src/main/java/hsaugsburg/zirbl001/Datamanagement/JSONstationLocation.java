1affd95caef84cbd55889b048714c6ac0a5e16f6
coList <list.corinna@outlook.de>
2017-06-27 17:05:01 +0200

    bugfixes

--- Zirbl001/app/src/main/java/hsaugsburg/zirbl001/Datamanagement/JSONstationLocation.java
+++ Zirbl001/app/src/main/java/hsaugsburg/zirbl001/Datamanagement/JSONstationLocation.java
@@ -19,104 +19,103 @@ import hsaugsburg.zirbl001.Models.StationLocationModel;
 import hsaugsburg.zirbl001.Models.StationModel;
 import hsaugsburg.zirbl001.NavigationActivities.Search.SearchActivity;
 
+public class JSONStationLocation extends AsyncTask<String, String, StationModel> {
+    private SearchActivity activity;
+    private int tourID;
+    private int stationID;
 
-public class JSONStationLocation extends AsyncTask<String, String, StationModel>{
-        private SearchActivity activity;
-        private int tourID;
-        private int stationID;
+    public JSONStationLocation (SearchActivity activity, int tourID, int stationID) {
+        this.activity = activity;
+        this.tourID = tourID;
+        this.stationID = stationID;
+    }
+    protected StationModel doInBackground(String... params) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
 
-        public JSONStationLocation (SearchActivity activity, int tourID, int stationID) {
-            this.activity = activity;
-            this.tourID = tourID;
-            this.stationID = stationID;
-        }
-        protected StationModel doInBackground(String... params) {
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
+        try {
+            URL url;
+            url = new URL(params[0]);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
 
-            try {
-                URL url;
-                url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
+            InputStream stream = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuffer buffer = new StringBuffer();
+            String line;
 
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuffer buffer = new StringBuffer();
-                String line;
-
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line);
-                }
-
-                String finalJson = buffer.toString();
-
-                try {
-                    JSONArray parentArray = new JSONArray(finalJson);
-                    JSONObject parentObject = parentArray.getJSONObject(0);
-
-                    JSONArray mJSONArrayStationLocations = parentObject.getJSONArray("stationlocations");
-
-                    StationLocationModel stationLocationModel = new StationLocationModel();
-
-
-                    for (int i = 0; i<mJSONArrayStationLocations.length()  ; i++) {
-                        JSONObject mJSONObjectStationDetails = mJSONArrayStationLocations.optJSONObject(i);
-
-                        if(mJSONObjectStationDetails.getInt("tourid") == tourID) {
-                            stationLocationModel.setTourID(mJSONObjectStationDetails.getInt("tourid"));
-                            stationLocationModel.setStationModel(mJSONObjectStationDetails.getJSONArray("stations"));
-                            Log.d("JSONStationLocation", "doInBackground: hello" + stationLocationModel.getStationModel());
-                        }
-                    }
-
-
-                    StationModel stationModel = new StationModel();
-                    JSONArray stations = stationLocationModel.getStationModel();
-                    
-                    for(int y=0; y<stations.length(); y++){
-                        JSONObject mJSONObjectStation = stations.optJSONObject(y);
-
-                        if(mJSONObjectStation.getInt("stationid") == stationID){
-                            stationModel.setTourID(mJSONObjectStation.getInt("tourid"));
-                            stationModel.setChronologyNumber(mJSONObjectStation.getInt("chronologynumber"));
-                            stationModel.setStationID(mJSONObjectStation.getInt("stationid"));
-                            stationModel.setLatitude(mJSONObjectStation.getDouble("latitude"));
-                            stationModel.setLongitude(mJSONObjectStation.getDouble("longitude"));
-                            stationModel.setStationName(mJSONObjectStation.getString("stationname"));
-                        }
-                    }
-                    return stationModel;
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
             }
-            return null;
-        }
-        protected void onPostExecute(StationModel result){
-            super.onPostExecute(result);
+
+            String finalJson = buffer.toString();
+
             try {
-                activity.processData(result);
+                JSONArray parentArray = new JSONArray(finalJson);
+                JSONObject parentObject = parentArray.getJSONObject(0);
+
+                JSONArray mJSONArrayStationLocations = parentObject.getJSONArray("stationlocations");
+
+                StationLocationModel stationLocationModel = new StationLocationModel();
+
+
+                for (int i = 0; i<mJSONArrayStationLocations.length()  ; i++) {
+                    JSONObject mJSONObjectStationDetails = mJSONArrayStationLocations.optJSONObject(i);
+
+                    if(mJSONObjectStationDetails.getInt("tourid") == tourID) {
+                        stationLocationModel.setTourID(mJSONObjectStationDetails.getInt("tourid"));
+                        stationLocationModel.setStationModel(mJSONObjectStationDetails.getJSONArray("stations"));
+                        Log.d("JSONStationLocation", "doInBackground: hello" + stationLocationModel.getStationModel());
+                    }
+                }
+
+
+                StationModel stationModel = new StationModel();
+                JSONArray stations = stationLocationModel.getStationModel();
+
+                for(int y=0; y<stations.length(); y++){
+                    JSONObject mJSONObjectStation = stations.optJSONObject(y);
+
+                    if(mJSONObjectStation.getInt("stationid") == stationID){
+                        stationModel.setTourID(mJSONObjectStation.getInt("tourid"));
+                        stationModel.setChronologyNumber(mJSONObjectStation.getInt("chronologynumber"));
+                        stationModel.setStationID(mJSONObjectStation.getInt("stationid"));
+                        stationModel.setLatitude(mJSONObjectStation.getDouble("latitude"));
+                        stationModel.setLongitude(mJSONObjectStation.getDouble("longitude"));
+                        stationModel.setStationName(mJSONObjectStation.getString("stationname"));
+                    }
+                }
+                return stationModel;
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
+        return null;
     }
+    protected void onPostExecute(StationModel result){
+        super.onPostExecute(result);
+        try {
+            activity.processData(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
 
 
