e516de1d12a82c267c9a02caeddc79b7fded0f96
Mel <melanie.behrens@hs-augsburg.de>
2017-06-27 16:21:20 +0200

    CleanUp Layout
    createButton
    Activity ClassRegistration

---
+++ Zirbl001/app/src/main/java/hsaugsburg/zirbl001/Fonts/QuicksandRegularPrimaryPicker.java
@@ -0,0 +1,52 @@
+package hsaugsburg.zirbl001.Fonts;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
+
+import biz.kasual.materialnumberpicker.MaterialNumberPicker;
+
+public class QuicksandRegularPrimaryPicker extends MaterialNumberPicker {
+
+    Typeface type;
+
+    public QuicksandRegularPrimaryPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    @Override
+    public void addView(View child) {
+        super.addView(child);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, int index,
+                        android.view.ViewGroup.LayoutParams params) {
+        super.addView(child, index, params);
+        type = Typeface.createFromAsset(getContext().getAssets(),
+                "fonts/Quicksand-Regular.ttf");
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, android.view.ViewGroup.LayoutParams params) {
+        super.addView(child, params);
+
+        type = Typeface.createFromAsset(getContext().getAssets(),
+                "fonts/Quicksand-Regular.ttf");
+        updateView(child);
+    }
+
+    private void updateView(View view) {
+
+        if (view instanceof EditText) {
+            ((EditText) view).setTypeface(type);
+        }
+
+    }
+}
