a2fa7198d408fa139890c814a3113a86c5e7f0d4
Mel <melanie.behrens@hs-augsburg.de>
2017-06-29 09:08:33 +0200

    Arrow and Color

--- Zirbl001/app/src/main/java/hsaugsburg/zirbl001/TourActivities/GoldenActivity.java
+++ Zirbl001/app/src/main/java/hsaugsburg/zirbl001/TourActivities/GoldenActivity.java
@@ -1,7 +1,9 @@
 package hsaugsburg.zirbl001.TourActivities;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import hsaugsburg.zirbl001.R;
 
@@ -17,5 +19,8 @@ public class GoldenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_golden);
+
+        TextView goldenPoints = (TextView) findViewById(R.id.goldenPoints);
+        goldenPoints.setTextColor(ContextCompat.getColor(this, R.color.colorGold));
     }
 }
