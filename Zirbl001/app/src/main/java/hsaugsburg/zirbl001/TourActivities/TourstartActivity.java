0934cfcb2a9ee6f28effd048ff23d85f491c86f2
tschaell <Tjuk@Tobiass-MBP.fritz.box>
2017-07-02 03:04:10 +0200

    changes

--- Zirbl001/app/src/main/java/hsaugsburg/zirbl001/TourActivities/TourstartActivity.java
+++ Zirbl001/app/src/main/java/hsaugsburg/zirbl001/TourActivities/TourstartActivity.java
@@ -23,10 +23,12 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -39,6 +41,9 @@ import hsaugsburg.zirbl001.NavigationActivities.QrCode.QrDialog;
 import hsaugsburg.zirbl001.Fonts.QuicksandRegularPrimaryEdit;
 
 import hsaugsburg.zirbl001.R;
+
+import static hsaugsburg.zirbl001.R.id.dotMenu;
+import static hsaugsburg.zirbl001.R.layout.layout_top_dark_actionbar;
 
 public class TourstartActivity extends AppCompatActivity {
 
@@ -62,6 +67,10 @@ public class TourstartActivity extends AppCompatActivity {
         return selectedTour;
     }
 
+    private TextView title;
+    private RelativeLayout dotMenuLayout;
+    private boolean dotMenuOpen = false;
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -72,8 +81,10 @@ public class TourstartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tourstart);
-        TextView title = (TextView) findViewById(R.id.titleActionbar);
+        title = (TextView) findViewById(R.id.titleActionbar);
         title.setText("Anmeldung");
+        dotMenuLayout=(RelativeLayout) this.findViewById(R.id.dotMenu);
+        dotMenuLayout.setVisibility(RelativeLayout.GONE);
 
 
         QuicksandRegularPrimaryEdit teamField = (QuicksandRegularPrimaryEdit) findViewById(R.id.teamname);
@@ -88,11 +99,34 @@ public class TourstartActivity extends AppCompatActivity {
 
         new JSONTourstart(this).execute(serverName + "/api/selectChronologyView.php");
 
+
     }
     
     public void showMenu(View view){
-        Log.d(TAG, "showMenu: click");
 
+        ImageView dotIcon = (ImageView) findViewById(R.id.dotIcon);
+        TextView menuStats = (TextView) findViewById(R.id.menuStats);
+        TextView menuQuit = (TextView) findViewById(R.id.menuQuit);
+
+        if(dotMenuOpen){
+            dotMenuLayout.setVisibility(RelativeLayout.GONE);
+            dotMenuOpen = false;
+            title.setTextColor(ContextCompat.getColor(mContext, R.color.colorAccent));
+            dotIcon.setColorFilter(ContextCompat.getColor(mContext, R.color.colorAccent));
+        } else {
+            dotMenuLayout.setVisibility(RelativeLayout.VISIBLE);
+            dotMenuOpen = true;
+            title.setTextColor(ContextCompat.getColor(mContext, R.color.colorTurquoise));
+            dotIcon.setColorFilter(ContextCompat.getColor(mContext, R.color.colorTurquoise));
+            menuQuit.setTextSize(18);
+            menuStats.setTextSize(18);
+        }
+    }
+    public void showStats(View view){
+        Log.d(TAG, "showStats: Stats");
+    }
+    public void quitTour(View view){
+        showEndTourDialog();
     }
 
 
