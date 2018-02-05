c1dfde7b04b13ae1d98d5c1c8e2926a6c94f7dc6
Mel <melanie.behrens@hs-augsburg.de>
2018-02-05 10:33:28 +0100

    Try to get ScrollViewState

--- Zirbl001/app/src/main/java/hsaugsburg/zirbl001/TourActivities/PointsActivity.java
+++ Zirbl001/app/src/main/java/hsaugsburg/zirbl001/TourActivities/PointsActivity.java
@@ -18,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.VideoView;
 
